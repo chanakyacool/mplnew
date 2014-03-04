a487e9176c93ed5afb321daa8932000bdf410c3e
bhanu <bhanu@bhanu.(none)>
2014-03-04 13:55:47 +0530

    score admin changed

--- app/admin/scores.rb
+++ app/admin/scores.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Score do
-  
+	form do |f|                         
+	    f.inputs "Scores" do       
+	      	f.input   :name
+		    f.input   :team1, as: :select, collection: Team.all 
+		    f.input   :team2, as: :select, collection: Team.all 
+		    f.input   :scoreteam1
+		    f.input   :scoreteam2
+		    f.input   :toss
+		    f.input   :venue
+		    f.input   :date   
+	    end                               
+	    f.actions                         
+	end    
 end
