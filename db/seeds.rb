ec13a352f14ec0e6f942317bc0ef081fffd25669
bhanu <bhanu@bhanu.(none)>
2014-03-05 12:04:01 +0530

    asiacup

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,116 +10,307 @@
 require 'rubygems'
 require 'faker'
 
-Score.create([{name: '1st'}, {name: '2nd'}, {name: '3rd'}, {name: '4th'},
-	 {name: '5th'}, {name: 'QF'}, {name: 'SF'}, {name: 'Final'}])
+#
+# ASIA CUP -- 2014
+#
+Team.create([{name: 'Pakistan'}, {name: 'Sri Lanka'}, {name: 'Bangladesh'}, {name: 'India'}, {name: 'Afghanistan'}])
 
-teams = Team.create([{name: 'India'}, {name: 'Australia'}, {name: 'England'}, {name: 'Pakistan'},
-	 {name: 'Zimbabwe'}, {name: 'South Africa'}, {name: 'West Indies'}, {name: 'Newzeland'}])
+
+Score.create([{name: '1st Match', team1: Team.find(1).name , team2: Team.find(2).name }, 
+	{name: '2nd Match', team1: Team.find(3).name , team2: Team.find(4).name }, 
+	{name: '3rd Match', team1: Team.find(5).name , team2: Team.find(1).name},
+	{name: '4th Match', team1: Team.find(4).name , team2: Team.find(2).name},
+	{name: '5th Match', team1: Team.find(3).name , team2: Team.find(5).name},
+	{name: '6th Match', team1: Team.find(4).name , team2: Team.find(1).name},
+	{name: '7th Match', team1: Team.find(5).name , team2: Team.find(2).name},
+	{name: '8th Match', team1: Team.find(4).name , team2: Team.find(1).name},
+	{name: '9th Match', team1: Team.find(5).name , team2: Team.find(4).name},
+	{name: '10th Match', team1: Team.find(3).name , team2: Team.find(2).name},
+	{name: 'Final Match', team1: Team.find(4).name , team2: Team.find(1).name}])
+
+# 1st Match
 
 11.times do 
-    Batting.create(score_id: 1, team_id: 1, innings: 1, name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 1, innings: 1, team_id: 1, name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 1, team_id: 2, innings: 1, name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 1, innings: 1, team_id: 2, name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 11.times do 
-    Batting.create(score_id: 1, team_id: 2, innings: 2, name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 1, innings: 2, team_id: 2, name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 1, team_id: 1, innings: 2, name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 1, innings: 2, team_id: 1, name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 
 
+# 2nd Match
+
 11.times do 
-    Batting.create(score_id: 2, team_id: 3, innings: 1, name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 2, innings: 1, team_id: 3, name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 2, team_id: 4, innings: 1, name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 2, innings: 1, team_id: 4, name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 11.times do 
-    Batting.create(score_id: 2, team_id: 4, innings: 2, name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 2, innings: 2, team_id: 4, name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 2, team_id: 3, innings: 2, name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 2, innings: 2, team_id: 3, name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 
 
+# 3rd Match
+
 11.times do 
-    Batting.create(score_id: 3, team_id: 1, innings: 1,name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 3, innings: 1, team_id: 5,name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 3, team_id: 4, innings: 1,name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 3, innings: 1, team_id: 1,name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 11.times do 
-    Batting.create(score_id: 3, team_id: 4, innings: 2,name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 3, innings: 2, team_id: 1,name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 3, team_id: 1, innings: 2,name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 3, innings: 2, team_id: 5,name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 
 
+# 4th Match
+
 11.times do 
-    Batting.create(score_id: 4, team_id: 2, innings: 1,name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 4, innings: 1, team_id: 4,name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 4, team_id: 4, innings: 1,name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 4, innings: 1, team_id: 3,name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 11.times do 
-    Batting.create(score_id: 4, team_id: 4, innings: 2,name: Faker::Name.name, bowler: Faker::Name.name , 
+    Batting.create(score_id: 4, innings: 2, team_id: 3,name: Faker::Name.name, bowler: Faker::Name.name , 
     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
 end
 
 5.times do
-	Bowler.create(score_id: 4, team_id: 2, innings: 2,name: Faker::Name.name, overs: Random.rand(1..6), 
+	Bowler.create(score_id: 4, innings: 2, team_id: 4,name: Faker::Name.name, overs: Random.rand(1..6), 
 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
 		wkts: Random.rand(1..2))
 end
 
 
-# 11.times do 
-#     Batting.create(team_id: 2, name: Faker::Name.name, bowler: Faker::Name.name , 
-#     	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
-#     	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
-# end
 
-# 5.times do
-# 	Bowler.create(team_id: 1, name: Faker::Name.name, overs: Random.rand(1..6),
-# 		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
-# 		wkts: Random.rand(1..2))
-# end
+# 5th Match
+
+11.times do 
+    Batting.create(score_id: 5, innings: 1, team_id: 3,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 5, innings: 1, team_id: 5,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+11.times do 
+    Batting.create(score_id: 5, innings: 2, team_id: 5,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 5, innings: 2, team_id: 3,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+
+
+# 6th Match
+
+11.times do 
+    Batting.create(score_id: 6, innings: 1, team_id: 4,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 6, innings: 1, team_id: 1,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+11.times do 
+    Batting.create(score_id: 6, innings: 2, team_id: 1,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 6, innings: 2, team_id: 4,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+
+
+# 7th Match
+
+11.times do 
+    Batting.create(score_id: 7, innings: 1, team_id: 5,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 7, innings: 1, team_id: 2,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+11.times do 
+    Batting.create(score_id: 7, innings: 2, team_id: 2,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 7, innings: 2, team_id: 5,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+
+
+# 8th Match
+
+11.times do 
+    Batting.create(score_id: 8, innings: 1, team_id: 3,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 8, innings: 1, team_id: 1,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+11.times do 
+    Batting.create(score_id: 8, innings: 2, team_id: 1,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 8, innings: 2, team_id: 3,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+
+
+# 9th Match
+
+11.times do 
+    Batting.create(score_id: 9, innings: 1, team_id: 5,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 9, innings: 1, team_id: 4,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+11.times do 
+    Batting.create(score_id: 9, innings: 2, team_id: 4,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 9, innings: 2, team_id: 5,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+
+
+# 10th Match
+
+11.times do 
+    Batting.create(score_id: 10, innings: 1, team_id: 3,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 10, innings: 1, team_id: 2,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+11.times do 
+    Batting.create(score_id: 10, innings: 2, team_id: 2,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 10, innings: 2, team_id: 3,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+
+
+# Final Match
+
+11.times do 
+    Batting.create(score_id: 11, innings: 1, team_id: 4,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 11, innings: 1, team_id: 1,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+11.times do 
+    Batting.create(score_id: 11, innings: 2, team_id: 1,name: Faker::Name.name, bowler: Faker::Name.name , 
+    	balls: Random.rand(20..65), runs: Random.rand(50..160), sixes: Random.rand(6..10), 
+    	fours: Random.rand(4..10), status: Random.rand(2) == 1 ? true : false)
+end
+
+5.times do
+	Bowler.create(score_id: 11, innings: 2, team_id: 4,name: Faker::Name.name, overs: Random.rand(1..6), 
+		wides: Random.rand(1..5), noball: Random.rand(2..5), maidens: Random.rand(1..3), 
+		wkts: Random.rand(1..2))
+end
+
